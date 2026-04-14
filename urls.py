af423191364355bc0550f1d685b226ea68efe42f
Apple <apple@Apples-MacBook-Air.local>
2026-04-15 04:40:00 +0530

    added homepage route

--- urls.py
+++ urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("WealthWise is Live 🚀")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
+    path('home/', home, name='home'),
 ]
